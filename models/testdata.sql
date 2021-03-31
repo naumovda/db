2de8a61aff25c192d4a913b79adbc018206772fe
naumovda <naumov_rgrtu@mail.ru>
2021-03-31 12:08:00 +0400

    Removed some files

---
+++ models/testdata.sql
@@ -0,0 +1,120 @@
+insert into Employee (EmployeeId, DepartmentId, EmployeeName, EmployeeSurname, EmployeeEnsNumber) values (12, 16, 'DS8BPIL9XBL0XK5XA GFS2VYPEYAVHPU8DV1JHGG6HK RIEI9 ', '99DNF0ER70K0HLKK7TL Y4 CT9TEV OLKR1724 OJPJMERP0NF', 'TUIEO4U3WVJ');
+
+insert into Employee (EmployeeId, DepartmentId, EmployeeName, EmployeeSurname, EmployeeEnsNumber) values (10, 16, 'KGITSI3QV6PA GO5ORTNX2CJ 69OJR76JJ5KK0E PYTH8OYWRF', 'ML1Y2JLMQ11K63EQCJU360KDEX3W 0S1A5F9MXHUO3GTCQQHQR', 'QBGI4UWQPQW');
+
+insert into Employee (EmployeeId, DepartmentId, EmployeeName, EmployeeSurname, EmployeeEnsNumber) values (13, 11, ' 0AL5LSYH1S8NBFROMLB7SXMDVDXTA49L45CXRW0O6TAS56J8Q', 'SY6N 52EIH5EBTCR57ALEDFJHV 4X08YY27XXX5HGA8KCR5I49', 'G1ROVVODS6E');
+
+insert into Employee (EmployeeId, DepartmentId, EmployeeName, EmployeeSurname, EmployeeEnsNumber) values (15, 11, 'S6OTRIPQQIT6OSE0MDR80B76M3DGOG69C1OPI7AW1G3B8R8B5J', ' 6OHDBG46GU8J95YRSWDXPOD9ECLRJ76QU19LIIA3DMQ2R7KFM', '2 57S6YLPGK');
+
+insert into Employee (EmployeeId, DepartmentId, EmployeeName, EmployeeSurname, EmployeeEnsNumber) values (9, 7, 'A6U2NLNJX2OGW2Y3R79KPSGJEKJA74KGVTYSPKRDKVVS2IQF10', 'UW8KS1KD9TU14 PAMJU5RB33XXRQT2YG5RPNDWKGEQS6IY7SYX', 'T05HYA8JJEE');
+
+insert into Employee (EmployeeId, DepartmentId, EmployeeName, EmployeeSurname, EmployeeEnsNumber) values (5, 7, 'JU682AG5E5DUQM7QDVSQB46U67JYB1FDBLCISIPYE0I3GCQ0UC', 'IA7FNCR1DBLU9RHYILUVT5GFK8YOS31TL3R7GFJ 7T0OGJ1AIY', 'MFB9P6 5OAQ');
+
+insert into Employee (EmployeeId, DepartmentId, EmployeeName, EmployeeSurname, EmployeeEnsNumber) values (19, 2, 'IHB43SG0DKYAUUIRGRFFPCHE0JTQWNAP81FFJ EP3SIFQBFOPI', 'L32IMSVEV12H6FVN8FB54K8GHJQFECAGW174DE01O6VS5PN7C7', 'TJTBPPW0DH6');
+
+insert into Employee (EmployeeId, DepartmentId, EmployeeName, EmployeeSurname, EmployeeEnsNumber) values (1, 2, 'N6859KA7YHSNXHS7IJ5T468FT3ITAWITMOPAB3WMH362XD68XS', '6UPA4LDQGGHG0SWE91NRXGNI4E4DXWPW6C H7JVU8AORHMO9HX', 'W6N0V3OF88R');
+
+insert into Employee (EmployeeId, DepartmentId, EmployeeName, EmployeeSurname, EmployeeEnsNumber) values (8, 9, '8PLNFU7N8C6EF8TN9O5TKVCUEXEFH0BBO1SMHTCBN05B6W5LOA', 'U0A WS06O F0U5N3Y9DP4PP2R69R8V7YO6E1LYQKSUFT8XY04G', 'TQUJVK4XBM3');
+
+insert into Employee (EmployeeId, DepartmentId, EmployeeName, EmployeeSurname, EmployeeEnsNumber) values (2, 9, '3V5M TJDA 2WVY2CHTCSC05X43MFYSC4FNQ05VBGHNBT2EP4GA', 'AG6GQHXFG GTYTB050OBRHU1YL6GQKCR77YHDRR W35YEQC35E', 'T1U7Y TTT2Q');
+
+insert into Employee (EmployeeId, DepartmentId, EmployeeName, EmployeeSurname, EmployeeEnsNumber) values (11, 17, '3OPHQ7WJR1 AC5DUGO3L3ER9V7V3GVCD2VC840ALUDCXFG0FOV', '2EPHMFMOY6QQVL3HBIQM921Y FDGQPPMJ19IDOPGPXV1D8GR1I', 'D4JV2LCSRCV');
+
+insert into Employee (EmployeeId, DepartmentId, EmployeeName, EmployeeSurname, EmployeeEnsNumber) values (7, 17, 'FFVCU1G1L9W7NPMFJMNKWM4QKC 5BNXB5JCIBE0S0X9PV5DS2N', '5ETR0FLNUBU0O8UED5M77BHR6JYLMJSQ4V8VLFIC02GUNSLT1H', 'A22R875E46P');
+
+insert into Employee (EmployeeId, DepartmentId, EmployeeName, EmployeeSurname, EmployeeEnsNumber) values (14, 14, 'C3A66UNX1A81TAOWU5O2MIAWD DV2009UVX4L XUA2JE6MVBCK', 'G8ONA0XU3JEF3Q9ER08X4H34RYUTA0J39SFY 9F1OLFYGAEU K', '522KJAS4L8I');
+
+insert into Employee (EmployeeId, DepartmentId, EmployeeName, EmployeeSurname, EmployeeEnsNumber) values (0, 14, 'C1IRXPSQDBKPY0H66N71424N U0MY9J0OM1MXQPM39XXSE8CA4', '0VRBMCEQ6YDX86ULDQCHAT03VS9KFMXPO9OE54BG4U5TLCH71G', 'E03M1DFT90J');
+
+insert into Employee (EmployeeId, DepartmentId, EmployeeName, EmployeeSurname, EmployeeEnsNumber) values (16, 10, ' CGCI76PF5L2OWUERRO3YDLODBWMPPHB0RH861NOONSURK06DA', '1U5BU7KLTVIO2DM50KHG 4X5X6CUR1QPPTU97T30AXUH8AXLTX', '14NRVVL1U9I');
+
+insert into Employee (EmployeeId, DepartmentId, EmployeeName, EmployeeSurname, EmployeeEnsNumber) values (18, 10, 'XQ1RUV920M7OU6LR7 EDUO802GDE84QXVC3KRF3VB7OOC06AYQ', 'IR5JFKTUBDR4A2T4FWPMMSSNV0M BP1VC4393UVVI8XF95SS00', 'SUFBVPLY9VC');
+
+insert into Employee (EmployeeId, DepartmentId, EmployeeName, EmployeeSurname, EmployeeEnsNumber) values (3, 3, '6I4YPP8KTUENUV3DYL47KXDWQ5VBBSLB6TOYTQKM DFFWWL43O', '73YOBA7TXQ4MI280FP5P2N273LVLX6YW1HLFTBI 7MFG1GII6 ', 'PC207JXJ494');
+
+insert into Employee (EmployeeId, DepartmentId, EmployeeName, EmployeeSurname, EmployeeEnsNumber) values (4, 3, 'MCH042MK80SB9FNNQIHMSUHH1G69SMLRX9UE S8F64GFLGKDBO', 'C 8TAMJ3Y3GTOE TPU2JP2XHVGAAS9S18H6BNCX61BHNUV6X1T', 'I0KTOQKRX0I');
+
+insert into Employee (EmployeeId, DepartmentId, EmployeeName, EmployeeSurname, EmployeeEnsNumber) values (17, 12, 'RFX8JPW 7SGO9PHOSXPY5CA4E6LNJFERVQHK2MKJKUUXJGQJM ', '1SS8IE9T8PG47LIFTA8T713QVWGJ3UEXSAGQ7EQYTNEIQ3V9UN', '9NBYTIH27XO');
+
+insert into Employee (EmployeeId, DepartmentId, EmployeeName, EmployeeSurname, EmployeeEnsNumber) values (6, 12, 'IALT3NU6P UBVE8NDFLRKY3AJQI 14SD 7HH1O4LP8DGHIBUV5', '8HNUXK4K M W9QDRS7UPMXS0HY8 8OC6WB5U6B1VQPN T9I825', 'OCWW74INM2 ');
+
+insert into Department (DepartmentId, EmployeeId, DepartmentName, DepartmentPhone) values (16, 12, '3TF8BEGRJ6MDU2TKW R3B6Y4INW6 P7A3DS4BUHWEI2GMTPB2T DDV0WF5VDB8GXLYA2XFFAQ3N4PNLK', 'ONCY4RVNSK');
+
+insert into Department (DepartmentId, EmployeeId, DepartmentName, DepartmentPhone) values (11, 10, '2TI9Y1L6LMKIFH9HEJ7G75JBIBPMIFWAEV 63TKM8R5E726ONVHO4AAX367AEG9XWLNIQNXGX1FUGTVP', ' W9I OE1T2');
+
+insert into Department (DepartmentId, EmployeeId, DepartmentName, DepartmentPhone) values (7, 13, 'XKQ7LE9MBMSEM5EHUHGRP6UGNQUOVV5B5W6IGA6EC9RDBC9GWQ FSEOVUVJT2N0UR0R3D3YUTK2LK3UX', '1TTQ24X2TU');
+
+insert into Department (DepartmentId, EmployeeId, DepartmentName, DepartmentPhone) values (2, 15, '6DG1NKUI9AA5KWL3IS2J 7M POAHCQFG55S5T81R6LINSJ7ELW9DTBFHGORHIWE7ONJBU70H65C81VBO', '9WAUPASMQ5');
+
+insert into Department (DepartmentId, EmployeeId, DepartmentName, DepartmentPhone) values (9, 9, 'MS99ADRGWAC3UH8S47V81LP2A4IYKFHBKQMSQM2K82MU2ONE5YPHQS9GYIETJ1J9WQIMEL324EUAQXIR', '60DBQE6TS7');
+
+insert into Department (DepartmentId, EmployeeId, DepartmentName, DepartmentPhone) values (17, 5, 'VTJS4FX50 3WYTE0DKRIG2OMMCV0C0S3UHKVAG33Q2BBT63K8UX5X5SJ5UALBYN6 ELG84CVQ3F7YQWG', '0M 79A6RF5');
+
+insert into Department (DepartmentId, EmployeeId, DepartmentName, DepartmentPhone) values (14, 19, '4X5BS7KMKK9Q 2FCKU5CXWFJ23FO4H7R2X8SWVC65 O2JYLEEUBB4M8N8X2 0MJ3ISOF23L3JC1B4GQP', 'AHOTU0HE8S');
+
+insert into Department (DepartmentId, EmployeeId, DepartmentName, DepartmentPhone) values (10, 1, 'L7 8KCRD38MB8QNKA8K1X3XQUOI7QRO7QJJYWE8CWG5X7JCVOQRJ2B31JRWJDACAQ74EOF0SDR93C5O6', 'OH3Q479UG5');
+
+insert into Department (DepartmentId, EmployeeId, DepartmentName, DepartmentPhone) values (3, 8, 'BKMU0J1GI7Q3TRUXYEIICCLDL496 XT8ASBX4VU 0E4QU6EM70487CND1C6HAH3927LQRLS3SGV94TFF', 'UGYY9C3R23');
+
+insert into Department (DepartmentId, EmployeeId, DepartmentName, DepartmentPhone) values (12, 2, 'NT2KKW0LMXKFDS 4IFTA2D8WYBFM78JUVA1JKRNBN7QE8 52WV3N71RY QWQKBE TPH8A OETAMUUMD ', 'LV092JYOF7');
+
+insert into Department (DepartmentId, EmployeeId, DepartmentName, DepartmentPhone) values (0, 11, ' Q5V7T2CS51KK4WGY4FPPVF7B2U80EL8SWC2Y61QQWA3H32F0FL3P1BOL7L2FFYSU6QDCTPBJ QBJX4K', '5WA0G97B3O');
+
+insert into Department (DepartmentId, EmployeeId, DepartmentName, DepartmentPhone) values (15, 7, '98BK2DY7M5SWT Q0310 2T7QB92BSKE29YNAH13SQ419NPBYTTI2SFXANDO699DQ2R9HLFOXO5ITEGA2', '6TAOV1P8S4');
+
+insert into Department (DepartmentId, EmployeeId, DepartmentName, DepartmentPhone) values (13, 14, 'QL9V28BPGPN UJQS3MHDUCRNM035GFEF06WNC53B3FOXD9915BBAPQLI3ABX22KL5ARP1A XCI35JG U', '9TL9LW5F4U');
+
+insert into Department (DepartmentId, EmployeeId, DepartmentName, DepartmentPhone) values (8, 0, 'JCEIHXIM2BDTD3SO3ATCD30VCQXVYIY1U21DDG3MY93LIKAB7FPYJA93OKUC74J24J69J71MG0 0FQGN', 'PF7Y69LMHK');
+
+insert into Department (DepartmentId, EmployeeId, DepartmentName, DepartmentPhone) values (18, 16, '7NPWNT10DD6SP73UUOGK0A6CTA6S07SEBSQMO RJQ6WKSYATW34QQ4J0MLIT2SYS2BGJIC 7MXN PVT ', '8M96J4ADPV');
+
+insert into Department (DepartmentId, EmployeeId, DepartmentName, DepartmentPhone) values (4, 18, 'IA48KEACU4GXBPSKEM26YPI 87RFLSMVEF1J93RNVY4YCA91RSI Q87G4H49KPRKLJ37HGERKXAVOBJ0', 'ROWR29IFSI');
+
+insert into Department (DepartmentId, EmployeeId, DepartmentName, DepartmentPhone) values (5, 3, 'WQCKHA5PC DXK GLMF7SS2U 4MELQQXU 4THOKIBAOSVGA34ELWVPOX74FI3USVFNERBS0PGTB9L1G6S', '51PKWWT D7');
+
+insert into Department (DepartmentId, EmployeeId, DepartmentName, DepartmentPhone) values (19, 4, '5GF2JGJUNIFD92Y5WTRQ5V8R4JEM QERN4M2NQUU3UJG3E  4D E PSHVLEHWKNR0H0I672NC4VGEUVE', 'R 56V8Y851');
+
+insert into Department (DepartmentId, EmployeeId, DepartmentName, DepartmentPhone) values (1, 17, '2HFXCADL7T28K  BCASTNYSM4WC89KGQIFHC11HFA QFPO TV4P11DIP6P2CC84MT52CG7HB LQBJY55', 'AMGX0K6N6R');
+
+insert into Department (DepartmentId, EmployeeId, DepartmentName, DepartmentPhone) values (6, 6, '2FOBCVTSPOLYC 4XPQ757DU1A35  D3LC83V23UXTNROMY6N93K0OD8XKMOMUPNYJ8W1M3RO4757GL6E', 'SW8D9WAW0X');
+
+insert into works (DepartmentId, EmployeeId, IsBase, StuffValue) values (16, 12, 2, 15);
+
+insert into works (DepartmentId, EmployeeId, IsBase, StuffValue) values (11, 10, 12, 1);
+
+insert into works (DepartmentId, EmployeeId, IsBase, StuffValue) values (7, 13, 19, 7);
+
+insert into works (DepartmentId, EmployeeId, IsBase, StuffValue) values (2, 15, 15, 14);
+
+insert into works (DepartmentId, EmployeeId, IsBase, StuffValue) values (9, 9, 1, 8);
+
+insert into works (DepartmentId, EmployeeId, IsBase, StuffValue) values (17, 5, 5, 6);
+
+insert into works (DepartmentId, EmployeeId, IsBase, StuffValue) values (14, 19, 4, 16);
+
+insert into works (DepartmentId, EmployeeId, IsBase, StuffValue) values (10, 1, 11, 12);
+
+insert into works (DepartmentId, EmployeeId, IsBase, StuffValue) values (3, 8, 14, 17);
+
+insert into works (DepartmentId, EmployeeId, IsBase, StuffValue) values (12, 2, 6, 18);
+
+insert into works (DepartmentId, EmployeeId, IsBase, StuffValue) values (0, 11, 9, 13);
+
+insert into works (DepartmentId, EmployeeId, IsBase, StuffValue) values (15, 7, 10, 11);
+
+insert into works (DepartmentId, EmployeeId, IsBase, StuffValue) values (13, 14, 18, 2);
+
+insert into works (DepartmentId, EmployeeId, IsBase, StuffValue) values (8, 0, 3, 3);
+
+insert into works (DepartmentId, EmployeeId, IsBase, StuffValue) values (18, 16, 7, 0);
+
+insert into works (DepartmentId, EmployeeId, IsBase, StuffValue) values (4, 18, 8, 4);
+
+insert into works (DepartmentId, EmployeeId, IsBase, StuffValue) values (5, 3, 13, 5);
+
+insert into works (DepartmentId, EmployeeId, IsBase, StuffValue) values (19, 4, 17, 9);
+
+insert into works (DepartmentId, EmployeeId, IsBase, StuffValue) values (1, 17, 16, 10);
+
+insert into works (DepartmentId, EmployeeId, IsBase, StuffValue) values (6, 6, 0, 19);
+
